70983cabfa9841d925508a946558ca9ab0ee2d54
Zoops <zoops0mania@gmail.com>
2022-04-23 19:00:49 +0300

    delete fig

--- js/script.js
+++ js/script.js
@@ -6,3 +6,20 @@ $(".header__nav ul a").on("click", function (e) {
   // 3
   $("html, body").animate({ scrollTop: $(href).offset().top }, 800);
 });
+
+// ДЛЯ МОДАЛЬНОГО ВИКНА
+var modal = document.getElementById('myModal');
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
